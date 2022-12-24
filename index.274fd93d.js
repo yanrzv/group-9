7518e4dc8a731d007a635aa7a910ee67681e2b35
yanrzv <116461898+yanrzv@users.noreply.github.com>
2022-12-24 03:42:51 +0000

    Deploying to gh-pages from @ yanrzv/group-9@9041ea80467ffa2fc4dc2bedc4f597ba140923e9 🚀

--- index.274fd93d.js
+++
@@ -1,2 +0,0 @@
-!function(){var n={openBtn:document.querySelector("[data-menu-open]"),btn:document.querySelector("[data-btn]")};n.openBtn.addEventListener("click",(function(){n.btn.classList.toggle("open")})),n.menuList.addEventListener("click",(function(){n.btn.classList.add("open")}))}();
-//# sourceMappingURL=index.274fd93d.js.map
